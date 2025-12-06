4f9b9d8ee0f377b15d15276ab854e02be251c15f
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2025-12-05 19:10:36 -0500

    Start nav bar

--- domdab/src/App.tsx
+++ domdab/src/App.tsx
@@ -1,12 +1,45 @@
-import './App.css'
+import "./App.css";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 function App() {
-
   return (
     <>
-    <p>Hello World</p>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>About</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+            <NavigationMenuTrigger>Work Experience</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+            <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+            <NavigationMenuTrigger>Volunteer</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+            <NavigationMenuTrigger>Hobbies</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <p>Hello World</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
