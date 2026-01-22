f6d6a34e482521962aae805f6669c6dc7b5783d0
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2026-01-22 13:39:29 -0500

    Pull NavBar into own component

--- domdab/src/components/NavBar.tsx
+++ domdab/src/components/NavBar.tsx
@@ -14,17 +14,11 @@ const NavBar = () => {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-<<<<<<< HEAD
-          <NavigationMenuTrigger><Link to="/">Dom</Link></NavigationMenuTrigger>
-          <NavigationMenuTrigger><Link to="/eng_history">Engineering Experience</Link></NavigationMenuTrigger>
-          <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-=======
           <NavigationMenuTrigger><Link to="/">Home</Link></NavigationMenuTrigger>
           <NavigationMenuTrigger><Link to="/eng_history">Eng. Experience</Link></NavigationMenuTrigger>
           <NavigationMenuTrigger>Eng. Projects</NavigationMenuTrigger>
           <NavigationMenuTrigger>Languages</NavigationMenuTrigger>
           <NavigationMenuTrigger>Previous Career</NavigationMenuTrigger>
->>>>>>> 5b570e3 (Add languages and previous career tabs to nav bar)
           <NavigationMenuTrigger>Volunteer</NavigationMenuTrigger>
           <NavigationMenuTrigger>Hobbies</NavigationMenuTrigger>
         </NavigationMenuItem>
