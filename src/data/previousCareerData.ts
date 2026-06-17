f3acab9f58b4316303f3f808a4d5fb32a7a21722
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2026-06-17 15:30:46 -0500

    Update copy

--- src/data/previousCareerData.ts
+++ src/data/previousCareerData.ts
@@ -6,7 +6,7 @@ import youtubeLogo from "@/assets/youtube-logo-png-46031.png"
 export const previousCareerData: Job[] = [
 {
     company: "Reddit",
-    description: "Safety Operations Specialist (Trust & Safety). Enforced and wrote content policy, banned bad actors at scale, and supported mitigating scaled abuse (spam).",
+    description: "Safety Operations Specialist (Trust & Safety). Served as the core strategic bridge balancing Operations, Policy, and Engineering teams during a phase of rapid platform scale-up. Ground-level collaborator in defining, testing, and shaping the internal content-review and moderation applications used daily by a growing global workforce. Researched and formulated the beginnings of Reddit’s Content Policy as well as enforcement guidelines",
     datesWorked: "May 2016 - April 2020",
     logoSource: redditLogo,
     logoHeight: "100%",
@@ -14,7 +14,7 @@ export const previousCareerData: Job[] = [
   },
   {
     company: "YouTube",
-    description: "Legal and Policy Associate. Enforced Terms of Service, resolved complaints from global entities including government and law enforcement with regards to defamation, trademark, counterfeit, and privacy. Contributed to policy focusing on free speech.",
+    description: "Legal and Policy Associate. Managed high-priority legal, privacy, and policy escalations globally, collaborating with law enforcement and regulatory organizations to mitigate platform risks. Contributed to policy focusing on free speech.",
     datesWorked: "Feb 2014 - May 2016",
     logoSource: youtubeLogo,
     logoHeight: "100%",
